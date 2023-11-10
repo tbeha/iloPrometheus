0eb3266d0a12b21f4dfe12131585c80e37a1b5f4
tbeha <t.beha@outlook.de>
2023-11-10 15:26:07 +0100

    u11102023

--- iloPromConnector.v4.0.py
+++ iloPromConnector.v4.0.py
@@ -1,4 +1,4 @@
- # Copyright 2020 Hewlett Packard Enterprise Development LP
+ # Copyright 2023 Hewlett Packard Enterprise Development LP
  #
  # Licensed under the Apache License, Version 2.0 (the "License"); you may
  # not use this file except in compliance with the License. You may obtain
@@ -14,16 +14,24 @@
  #
  # v2.1 - August 2022; Thomas Beha, HPE Technology Competence Center DACH
  #        added the temperature state = enabled filter
+ # v3  -  August 2022, Thomas Beha, HPE Technology Competence Center DACH
+ #        added Rack Location
+ # v3.1 - October 2022; Thomas Beha, HPE Technology Competence Center DACH
+ #        Minor Bug fixes
+ # v3.2 - May 2023, Thomas Beha, HPE Technology Competence Center DACH
+ #        Changes in the program flow, to better handle removed server
+ # v4.0 - October 2023, Thomas Beha, HPE Technology Competence Center DACH
+ #        Data Collector writing the data into Redis Lists
+ #        A second data analyzer component will publish the data to Prometheus
 
 # -*- coding: utf-8 -*-
-from math import log1p
 from cryptography.fernet import *
 from lxml import etree 
 import time
 from datetime import datetime
 from prometheus_client import Counter, Gauge, start_http_server, Info
 import sys
-from redfish import redfish_client
+import redfish
 
 BtoGB=pow(1024,3)
 BtoMB=pow(1024,2)
@@ -34,6 +42,7 @@ power_state={
     'Off': 2,
     'Suspended': 3
 }
+
 
 def logwriter(f, text):
         output=str(datetime.today()) +": "+text+" \n"
@@ -52,7 +61,7 @@ def logclose(f):
 def get_resource_directory(redfishobj, lfile):
 
     try:
-        resource_uri = redfishobj.root.obj.Oem.Hpe.Links.ResourceDirectory['@odata.id']
+        resource_uri = redfishobj.root['@odata.id']+'ResourceDirectory/'
         response = redfishobj.get(resource_uri)
         return response.dict["Instances"]
     except KeyError as ex:
@@ -63,53 +72,44 @@ def get_resource_directory(redfishobj, lfile):
         logclose(log)
         return None
 
-
-def get_server_urls( login_account, login_password, ilos, lfile):
+def get_server_urls( login_account, login_password, server, lfile):
 
     server_urls={}
 
-    for ilo in ilos:
-        server={}
-        ilo_url="https://"+ilo.text
-        server["url"]=ilo_url
+    for s in server:
+        ilo_url="https://"+s['ilo']
+        s["url"]=ilo_url
         try:
             # Create a Redfish client object
-            REDFISHOBJ = RedfishClient(base_url=ilo_url, username=login_account, password=login_password)
+            REDFISHOBJ = redfish.redfish_client(base_url=ilo_url, username=login_account, password=login_password)  
             # Login with the Redfish client
             REDFISHOBJ.login()
-            server["redfish"]=REDFISHOBJ
+            s["redfish"]=REDFISHOBJ
             resource_instances = get_resource_directory(REDFISHOBJ, lfile)
-            REDFISHOBJ.logout()
             for instance in resource_instances:
-                if '#ComputerSystem.' in instance['@odata.type']:
-                    server["ComputerSystem"]=instance['@odata.id'] 
+                if '#ComputerSystem.' in instance ['@odata.type']:
+                    s["ComputerSystem"]=instance['@odata.id'] 
                 if '#Power.' in instance ['@odata.type']:
-                    server["Power"]=instance['@odata.id'] 
+                    s["Power"]=instance['@odata.id'] 
                 if '#Thermal.' in instance ['@odata.type']:
-                    server["Thermal"]=instance['@odata.id']
-            if len(server)> 2: 
-                server_urls[ilo.text]=server
+                    s["Thermal"]=instance['@odata.id']
+            if len(s) > 4:
+                server_urls[s['ilo']]=s
         except Exception as ex:
             log=logopen(lfile)
-            logwriter(log,'Exception - get_server_urls: '+ilo.text)
-            logwriter(log,str(ex))
+            logwriter(log,'Exception - get_server_urls: '+s['ilo'])
+            logwriter(log,str(ex.__context__))
             logclose(log)
-            if len(server)> 2:
-                server_urls[ilo.text]=server
+            if len(s) > 4:
+                server_urls[s['ilo']]=s
             pass
     return server_urls;
 
 def get_server_data( login_account, login_password, server, lfile):
 
-    log = logopen(lfile)
-    logwriter(log,"Debug - Get Server Data: "+server["url"])
-    logclose(log)
     server_data={}
     try:
-        # Create a Redfish client object
-        REDFISHOBJ = RedfishClient(base_url=server["url"], username=login_account, password=login_password)
-        # Login with the Redfish client
-        REDFISHOBJ.login()
+        REDFISHOBJ = server["redfish"]
         # System Data
         server_data['System'] = REDFISHOBJ.get(server["ComputerSystem"]).obj
         # Power Data
@@ -117,43 +117,39 @@ def get_server_data( login_account, login_password, server, lfile):
         server_data["PowerMeter"] = REDFISHOBJ.get(uri).obj
         # Temperatures
         server_data['Temperatures'] = REDFISHOBJ.get(server["Thermal"]).obj["Temperatures"]
-        # logout
-        REDFISHOBJ.logout()
         return server_data;
     
     except Exception as ex:
         sys.stderr.write("ERROR during get_server_date: "+server["url"])
+        if ex.status == 401:
+            REDFISHOBJ = RedfishClient(base_url=server["url"], username=login_account, password=login_password)
+            REDFISHOBJ.login()
+            server["redfish"] = REDFISHOBJ
         log=logopen(lfile)
         logwriter(log,'Exception')
-        logwriter(log,str(ex))
+        logwriter(log,str(ex.__context__))
         logclose(log)        
         pass
 
-def display_results( node, inode, server_metrics, server_ip):
+def display_results( node, inode, server_metrics, server):
     hostname = (server_metrics['System']['HostName']).split('.')[0].replace('-','_')
-    cn = server_ip
-    log = logopen(lfile)
-    logwriter(log,"Debug - Display "+cn)
-    logclose(log)
+    cn = server['ilo']
     inode.labels(cn).info({"Model":server_metrics['System']["Model"],"Manufacturer":server_metrics['System']["Manufacturer"],"SerialNumber":server_metrics['System']["SerialNumber"],"Hostname":hostname})
-    node.labels(cn,'Power','State').set(power_state[server_metrics['System']["PowerState"]]) 
-    node.labels(cn,'Power','Average').set(server_metrics["PowerMeter"]['Average'])           
-    node.labels(cn,'Power','Maximum').set(server_metrics["PowerMeter"]['Maximum'])
-    node.labels(cn,'Power','Minimum').set(server_metrics["PowerMeter"]['Minimum'])
+    node.labels(cn,server['Rack'],'Power','State').set(power_state[server_metrics['System']["PowerState"]]) 
+    node.labels(cn,server['Rack'],'Power','Average').set(server_metrics["PowerMeter"]['Average'])           
+    node.labels(cn,server['Rack'],'Power','Maximum').set(server_metrics["PowerMeter"]['Maximum'])
+    node.labels(cn,server['Rack'],'Power','Minimum').set(server_metrics["PowerMeter"]['Minimum'])
     for temperature in server_metrics['Temperatures']:
         if temperature['Status']['State'] == 'Enabled': 
-            node.labels(cn,'Temperature',temperature["Name"]).set(temperature['ReadingCelsius'])
+            node.labels(cn,server['Rack'],'Temperature',temperature["Name"]).set(temperature['ReadingCelsius'])
     return 0
 
-
-### Main ###########################################################################
-
 if __name__ == "__main__":
-    
+
     """ read the key and input file""" 
 
-    path = '.'
-    #path = '/opt/prometheus/data' 
+    #path = '.'
+    path = '/opt/prometheus/data' 
     keyfile = path + '/iloprometheus.key'  
     xmlfile = path + '/iloprometheus.xml'
 
@@ -173,52 +169,40 @@ if __name__ == "__main__":
     lfile=path+(tree.find("logfile")).text
     port=int((tree.find("port")).text)
     mintervall = int((tree.find("monitoringintervall")).text) 
-    ilos=tree.findall("ILO_ip")  # List of all ILOs
+    #sx=tree.findall("server")  # List of all server
+    server=[]
+    for s in tree.findall("server"):
+        server.append({"ilo":s.find("ILO_ip").text,"Rack":s.find("Rack").text,"Loc":s.find("Loc").text})
 
     # Get the monitoring URLs of the server
-    monitor_urls = get_server_urls(user, password, ilos, lfile)
-    log = logopen(lfile)
-    logwriter(log,"Debug - monitor_urls")
-    logwriter(log,str(monitor_urls))
-    logclose(log)
+    monitor_urls = get_server_urls(user, password, server, lfile)
 
     # open the logfile
     log=logopen(lfile)
     logwriter(log,"Started ILO Prometheus Connector Test")
-    
+
     # Start the http_server and the counters, gauges
     start_http_server(port)
     c = Counter('ilorest_sample','ILO REST sample number')
-    node = Gauge('ilorest_node','ILO Node Data',['nodename','nodemetric','metricdetail'])
+    node = Gauge('ilorest_node','ILO Node Data',['nodename','rack','nodemetric','metricdetail'])
     delta = Gauge('ConnectorRuntime','Time required for last data collection in seconds')
     inode = Info('node','Additional Node Info',['node'])
 
     # Start the endless loop
-    while True:
-        try:
-            t0 = time.time()         
-            c.inc()
-            for server in monitor_urls:
+    while True: 
+        t0 = time.time()         
+        for server in monitor_urls:
+            try:
                 server_metrics = get_server_data(user, password, monitor_urls[server], lfile)
-                display_results(node, inode, server_metrics, server)
-            t1 = time.time()
-            delta.set((t1-t0))
-            while ((t1-t0) < mintervall):
-                time.sleep(1.0)
-                t1 = time.time()    
-
-        except Exception as ex:
-            print(ex)
-            log=logopen(lfile)
-            logwriter(log,'Exception')
-            logwriter(log,str(ex))
-            logclose(log)
-            pass            
-
-
-
-
-
-
-    print("The end")    
-
+                display_results(node, inode, server_metrics, monitor_urls[server])
+            except Exception as ex:
+                log=logopen(lfile)
+                logwriter(log,'Exception')
+                logwriter(log,str(ex.__context__))
+                logclose(log)
+                pass
+        t1 = time.time()
+        delta.set((t1-t0))
+        while ((t1-t0) < mintervall):
+            time.sleep(1.0)
+            t1 = time.time()    
